7742322edbed73a43c80294238441a7492e334a0
xy1xy <2442596+xy1xy@users.noreply.github.com>
2021-07-21 00:01:15 +1000

    Add Logger method JSDocs to definition file

--- index.d.ts
+++ index.d.ts
@@ -27,9 +27,27 @@ export interface SeqEvent {
 
 export declare class Logger {
   constructor (config: SeqLoggerConfig)
-
+  /**
+   * Enqueue an event in Seq format.
+   * @param {*} event
+   * @returns {void}
+   */
   emit (event: SeqEvent): void
+  /**
+   * Flush then destroy connections, close the logger, destroying timers and other resources.
+   * @returns {Promise<void>}
+   */
   close (): Promise<void>
+  /**
+   * Flush events queued at the time of the call, and wait for pending writes to complete regardless of configured batching/timers.
+   * @returns {Promise<boolean}
+   */
   flush (): Promise<boolean>
+  /**
+   * * A browser only function that queues events for sending using the navigator.sendBeacon() API.
+   * * This may work in an unload or pagehide event handler when a normal flush() would not.
+   * * Events over 63K in length are discarded (with a warning sent in its place) and the total size batch will be no more than 63K in length.
+   * @returns {boolean}
+   */
   flushToBeacon (): boolean
 }
