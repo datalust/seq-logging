0ed649585edba97031ad645edc87a61bb6ce947b
Ashley Mannix <ashleymannix@live.com.au>
2025-05-06 13:04:53 +1000

    fix up fallback module imports

--- browser.js
+++ browser.js
@@ -1,5 +1,13 @@
 "use strict";
 
-const Logger = (await import('./seq_logger.js')).DefineLogger(Blob, fetch, typeof AbortController !== 'undefined' ? AbortController : await import('abort-controller'));
+// Variable used to force testing fallback modules
+// This should remain false, but can be changed in testing
+const fallback = false;
+
+const Logger = (await import('./seq_logger.js')).DefineLogger(
+    Blob,
+    fetch,
+    !fallback && typeof AbortController !== 'undefined' ? AbortController : (await import('abort-controller')).AbortController
+);
 
 export { Logger };
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 "use strict";
 
+// Variable used to force testing fallback modules
+// This should remain false, but can be changed in testing
+const fallback = false;
+
 const Logger = (await import('./seq_logger.js')).DefineLogger(
-    typeof Blob !== 'undefined' ? Blob : (await import('buffer')).Blob,
-    typeof fetch !== 'undefined' ? fetch : await import('node-fetch'),
-    typeof AbortController !== 'undefined' ? AbortController : await import('abort-controller')
+    !fallback && typeof Blob !== 'undefined' ? Blob : (await import('buffer')).Blob,
+    !fallback && typeof fetch !== 'undefined' ? fetch : (await import('node-fetch')).default,
+    !fallback && typeof AbortController !== 'undefined' ? AbortController : (await import('abort-controller')).AbortController
 );
 
 export { Logger };
