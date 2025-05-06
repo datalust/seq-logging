21ff125ff56a227c70918d2fdccb04ba3834af77
Ashley Mannix <ashleymannix@live.com.au>
2025-05-06 12:36:13 +1000

    fix up browser import

--- browser.js
+++ browser.js
@@ -1,5 +1,5 @@
 "use strict";
 
-const Logger = (await import('./seq_logger')).DefineLogger(Blob, fetch, typeof AbortController !== 'undefined' ? AbortController : await import('abort-controller'));
+const Logger = (await import('./seq_logger.js')).DefineLogger(Blob, fetch, typeof AbortController !== 'undefined' ? AbortController : await import('abort-controller'));
 
 export { Logger };
