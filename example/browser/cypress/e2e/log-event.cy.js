66bef9e1f8da024b9a3c920057015018c8fac3ee
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2025-07-23 11:43:12 +1000

    Revert attempted test fix

--- example/browser/cypress/e2e/log-event.cy.js
+++ example/browser/cypress/e2e/log-event.cy.js
@@ -6,8 +6,7 @@ context('The app', () => {
     it('logs to Seq', () => {
         cy.get('#log-event').click();
 
-        // This appears to be flaky on slower runners.
-        // cy.get('#status').should('have.text', 'Logging an event...');
+        cy.get('#status').should('have.text', 'Logging an event...');
 
         // After a little while...
 
